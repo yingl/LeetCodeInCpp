b46ebe0e30e3f6a8758a32c7f6097eea8c950d46
林应 <linying@linyingdeMacBook-Pro.local>
2014-04-07 13:47:32 +0800

    帮助本地检查编译错误

---
+++ compile_check_inc.h
@@ -0,0 +1,22 @@
+#include <algorithm>
+#include <cstdlib>
+#include <map>
+#include <set>
+#include <stack>
+#include <string>
+#include <vector>
+
+using namespace std;
+
+ struct ListNode {
+  int val;
+  ListNode *next;
+  ListNode(int x) : val(x), next(NULL) {}
+ };
+
+struct TreeNode {
+  int val;
+  TreeNode *left;
+  TreeNode *right;
+  TreeNode(int x) : val(x), left(NULL), right(NULL) {}
+ };
