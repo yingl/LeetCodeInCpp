0b95b9af6d89e9de63b54cf649ba12f77db139a8
林应 <linying@217.24.97.119.broad.wh.hb.dynamic.163data.com.cn>
2014-04-25 22:23:19 +0800

    Binary Tree Level Order Traversal

---
+++ binary_tree_level_order_traversal.cpp
@@ -0,0 +1,40 @@
+class Solution {
+public:
+  vector<vector<int> > levelOrder(TreeNode *root) {
+    vector<vector<int> > result;
+    queue<TreeNode*> queues[2];
+    int curr_queue = 0;
+    int next_queue = 1;
+
+    if (NULL == root) {
+      return result;
+    }
+
+    queues[curr_queue].push(root);
+    
+    while (!queues[curr_queue].empty()) {
+      vector<int> level;
+
+      while (!queues[curr_queue].empty()) {
+        TreeNode *node = queues[curr_queue].front();
+
+        queues[curr_queue].pop();
+
+        if (node->left != NULL) {
+          queues[next_queue].push(node->left);
+        }
+
+        if (node->right != NULL) {
+          queues[next_queue].push(node->right);
+        }
+
+        level.push_back(node->val);
+      }
+
+      result.push_back(level);
+      swap(curr_queue, next_queue);
+    }
+
+    return result;
+  }
+};
