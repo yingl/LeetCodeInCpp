498291cf61ae50a214850f0b3985a7a09e593ef0
林应 <linying@linyingdeMacBook-Pro.local>
2014-04-29 15:53:40 +0800

    Pascal's Triangle II

---
+++ pascals_triangle_ii.cpp
@@ -0,0 +1,14 @@
+class Solution {
+public:
+  vector<int> getRow(int rowIndex) {
+    vector<int> row(rowIndex + 1, 1);
+
+    for (int i = 1; i <= rowIndex; ++i) {
+      for (int j = i - 1; j > 0; --j) {
+        row[j] += row[j - 1];
+      }
+    }
+
+    return row;
+  }
+};
