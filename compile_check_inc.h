3c0d81b55b127169a8db46c9ef6fb14a24572732
yingl <linying_43151@163.com>
2014-05-05 00:27:25 +0800

    添加包含unordered_map/set头文件

--- compile_check_inc.h
+++ compile_check_inc.h
@@ -2,9 +2,12 @@
 #include <cstdlib>
 #include <iostream>
 #include <map>
+#include <queue>
 #include <set>
 #include <stack>
 #include <string>
+#include <unordered_map>
+#include <unordered_set>
 #include <vector>
 
 using namespace std;
