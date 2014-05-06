8d2fcddecbb021c9be738d2aa2b0afedf1695792
yingl <linying_43151@163.com>
2014-05-06 23:46:27 +0800

    给出Gas Station的证明

--- gas_station.cpp
+++ gas_station.cpp
@@ -1,7 +1,10 @@
 class Solution {
 public:
   // 这里有一个重要的假设，如果从某个a点出发到b点失败，
-  // 那么a到b任何一点都不能成功。证明~~~自己google之。
+  // 那么a到b任何一点都不能成功。证明如下：
+  //   1. 如果能从a点加油后先开到a1点，剩余的汽油必定大于等于0。
+  //   2. 如果放弃a点，那么从a1的出发的油量不会更多。
+  //   3. 同理可以一直推理下去。
   int canCompleteCircuit(vector<int> &gas, vector<int> &cost) {
     int number = gas.size();
 
